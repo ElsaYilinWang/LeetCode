57654e30ec8e53d2e9e11d18bbb62a3b5970485a
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-02-25 17:40:17 +0000

    add 217.py and 217.java

--- 217.java
+++ 217.java
@@ -2,7 +2,7 @@
 //Reference: https://leetcode.com/problems/contains-duplicate/solutions/3672475/4-method-s-c-java-python-beginner-friendly/?envType=featured-list&envId=top-interview-questions?envType=featured-list&envId=top-interview-questions
 
 import java.util.HashSet;
-
+// HashSet
 class Solution {
     public boolean containsDuplicate(int[] nums){
         HashSet<Integer> seen = new HashSet<>();
