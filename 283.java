429257fd077fe67ceeea0204003a503b178b16f1
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-03-03 12:42:31 +0000

    add 283.py & 283.java

---
+++ 283.java
@@ -0,0 +1,24 @@
+//LeetCode 283 Move Zeroes
+// Reference: https://www.youtube.com/watch?v=k5lIW5XxC7g
+
+
+class Solution{
+    public void moveZeroes(int[] nums){
+        // Start with the first position
+        int insertPosition = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            //Move all non-zero integers to left side
+            if(nums[i] != 0){
+                nums[insertPosition] = nums[i];
+                insertPosition ++;
+            }
+        }
+
+        while (insertPosition < nums.length){
+            nums[insertPosition ++] = 0;
+        }
+    }
+
+}
+
---
+++ 283.py
@@ -0,0 +1,16 @@
+# LeetCode 283 Move Zeroes
+# Reference: https://www.youtube.com/watch?v=aayNRwUN3Do
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # QuickSort
+        l = 0
+        for r in range(len(nums)):
+            if nums[r]: # nums[r] == 0
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+        return nums
+        
