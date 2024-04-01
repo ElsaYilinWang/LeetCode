5ab80c099f504be273f6172a798b70d19b16ee62
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-04-01 22:53:30 +0100

    15 3Sum

---
+++ 15.java
@@ -0,0 +1,59 @@
+// LeetCode 15 3Sum
+// Reference: https://leetcode.com/problems/3sum/solutions/3736346/java-easiest-solution-ever/?envType=featured-list&envId=top-interview-questions?envType=featured-list&envId=top-interview-questions
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        //Sort the array
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++){
+            //Skip dubplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0){
+                    //Found a triplet with zero sum
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    //Skip dublicate elements for j
+                    while (j < k && nums[j] == nums[j + 1]){
+                        j ++;
+
+                    }
+
+                    // Skip duplicate elements for k
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    // Move the pointers
+                    j++;
+                    k--;
+
+                } else if (sum < 0) {
+                    // Sum is less than zero, increment j to increase the sum
+                    j++;
+                } else {
+                    // Sum is greater than zero, decrement k to decrease the sum
+                    k--;
+                }
+            }
+
+        }
+        
+        return ans;
+    }
+}
