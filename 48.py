6ea31741d8982da727d5b4f92eaa91e279deea5f
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-04-08 21:24:30 +0100

    LeetCode 48 Practice

---
+++ 48.py
@@ -0,0 +1,31 @@
+# LeetCode 48 Rotate Image
+# Reference:https://www.youtube.com/watch?v=fMSJSS7eO1w
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l, r = 0, len(matrix) - 1
+
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+
+                # save the topLeft
+                topLeft = matrix[top][l + 1]
+
+                # move bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+
+                # move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[top + i][r]
+
+                # move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+
+                # move top left into top right
+                matrix[top + i][r] = topLeft
+
+            r -= 1
+            l += 1
+        
