6f74b70198a63cef68a410e0e6b5409d76ecf0da
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-04-15 23:49:22 +0100

    leetcode 75 java update

--- 75.java
+++ 75.java
@@ -1,5 +1,33 @@
 // LeetCode 75 Sort Colors
-// Reference: 
+// Reference: https://leetcode.com/problems/sort-colors/solutions/3164333/solution/?envType=featured-list&envId=top-interview-questions?envType=featured-list&envId=top-interview-questions
+
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int l = 0;
+        int r = nums.length - 1;
+
+        for (int i = 0; i <= r;){
+            if (nums[i] == 0){
+                swap(nums, i++, l++);
+
+            } else if (nums[i] == 1){
+                ++i;
+
+            } else {
+                swap(nums, i, r--);
+            }
+
+        }
+        
+        
+    }
+    private void swap(int[] nums, int i, int j){
+        final int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;       
+    }
+}
 
 
 
