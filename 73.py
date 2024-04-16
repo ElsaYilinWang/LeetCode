55595f6bf7f84c9435b3691745324f38e8b6b03a
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-04-16 13:01:28 +0100

    Leetcode 73 Practice

---
+++ 73.py
@@ -0,0 +1,33 @@
+# LeetCode 73 Set Matrix Zeroes
+# Reference: https://www.youtube.com/watch?v=T41rL0L3Pnw
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # O(1)
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+
+        # determine which rows/cols need to be zero
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+        
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0
