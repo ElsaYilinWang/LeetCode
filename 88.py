cc7dfc46ca899de6cf608c1b1e8ecd09546e6372
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-02 18:50:11 +0100

    Update 88.py

--- 88.py
+++ 88.py
@@ -1,3 +1,6 @@
+# Leetcode 88 Merged Sorted Array
+# Reference: https://www.youtube.com/watch?v=P1Ic85RarKY
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
@@ -7,7 +10,7 @@ class Solution:
         last = m + n - 1
 
         # Fill in reverse order
-        while m > 0 and n > 0:
+        while m > 0 and n > 0: # while there are elements in both nums1 and nums2
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[last] = nums1[m - 1]
                 m -= 1
