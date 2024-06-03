27e3ba3dc34448b19065d774fee605c7b043f177
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-03 12:48:03 +0100

    update 80.java

--- 80.java
+++ 80.java
@@ -1,3 +1,21 @@
 // LeetCode 80 Remove Duplicates from Sorted Array II
-// Reference:
+// Reference: https://www.youtube.com/watch?v=OZaADxYTfD4
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        // TIme O(N)
+        // Spce O(1)
+        int in = 2;
+
+        if (nums.length < 3) return nums.length;
+
+        for (int i = 2; i < nums.length; i++){
+            if (nums[i] != nums[in-2]){
+                nums[in] = nums[i];
+                in ++;
+            }
+        }
+        return in;
+    }
+}
 
