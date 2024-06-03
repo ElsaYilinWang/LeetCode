f26f46bae019b63b44490b2805ee8a9dba70020a
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-03 12:51:11 +0100

    update 80.js

--- 80.js
+++ 80.js
@@ -1,5 +1,20 @@
 // LeetCode 80 Remove Duplicates from Sorted Array II
-// Reference: 
+// Reference: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let j = 2;
 
+    for (let i = 2; i < nums.length; i++){
+        if (nums[i] !== nums[j-2]){
+            nums[j++] = nums[i];
+        }
+    }
+
+    return j;
+    
+};
         
