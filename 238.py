2a336c35a2e5613bf4b41fd203ae0375b3a48275
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-05 13:51:53 +0100

    LeetCode 238 Practice

---
+++ 238.py
@@ -0,0 +1,18 @@
+# LeetCode 238 Product of Array Except Self
+# Reference: https://www.youtube.com/watch?v=bNvIQI2wAjk
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
