b82bcc4cc3787fa8141f524c5fc95a84c8529b8c
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-19 15:00:16 +0100

    LeetCode 57 Practice

---
+++ 57.py
@@ -0,0 +1,21 @@
+# LeetCode 57 Insert Interval
+# Reference:https://www.youtube.com/watch?v=A8NUOmlwOlM
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        
+        res.append(newInterval)
+        return res
