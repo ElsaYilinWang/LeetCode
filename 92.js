9c42bf9be3a0e2ede56fc416533824a1f09bf864
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-24 10:14:15 +0100

    LeetCode 92 Practice

---
+++ 92.js
@@ -0,0 +1,36 @@
+// LeetCode 92 Reverse Linked List II
+// References:https://leetcode.com/problems/reverse-linked-list-ii/solutions/4011862/92-40-two-pointers-stack-recursion/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+    if (!head || left === right) return head;
+
+    const dummy = new ListNode(0);
+    dummy.next = head;
+    let prev = dummy;
+
+    for (let i = 0; i < left - 1; ++i){
+        prev = prev.next;
+    }
+    let current = prev.next;
+
+    for (let i = 0; i < right - left; ++i){
+        const nextNode = current.next;
+        current.next = nextNode.next;
+        nextNode.next = prev.next;
+        prev.next = nextNode;
+    }
+    return dummy.next;
+};
