50c579d6dbe417471e7d0be8be6c9645f339221e
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-06-26 11:54:53 +0100

    LeetCode 226 Practice

---
+++ 226.js
@@ -0,0 +1,30 @@
+// LeetCode 226 Invert Binary Tree
+// References:https://leetcode.com/problems/invert-binary-tree/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    // Base case:
+    if (root == null) return root;
+
+    // Call the function recursively for the left subtree...
+    invertTree(root.left)
+    // Call the function recursively for the right subtree...
+    invertTree(root.right)
+    // swapping process...
+    const curr = root.left
+    root.left = root.right
+    root.right = curr
+    return root         // Return the root... 
+    
+};
---
+++ 226.py
@@ -0,0 +1,22 @@
+# LeetCode 226 Invert Binary Tree
+# References:https://www.youtube.com/watch?v=OnSn2XEQ4MY
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        # swap the children
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
