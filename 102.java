269d98270012a629f980e1ab72747eb2b03d6cdc
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-07-02 12:04:57 +0100

    Leetcode 102

---
+++ 102.java
@@ -0,0 +1,42 @@
+// Leetcode 102: Binary Tree Level Order Traversal - BFS
+// References:https://leetcode.com/problems/binary-tree-level-order-traversal/solutions/3267417/well-explained-code-in-java-use-of-arraylist-and-queues/?envType=study-plan-v2&envId=top-interview-150
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> finalAns = new ArrayList<List<Integer>>();
+        if(root==null){
+            return finalAns;
+        }
+        q.add(root);
+        while(!q.isEmpty()){
+            int levels = q.size();
+            List<Integer> subLevels = new ArrayList<>();
+            for(int i=0;i<levels;i++){
+                if(q.peek().left!=null){
+                    q.add(q.peek().left);
+                }
+                if(q.peek().right!=null){
+                    q.add(q.peek().right);
+                }
+                subLevels.add(q.remove().val);
+            }
+            finalAns.add(subLevels);
+        }
+        return finalAns;
+    }
+}
